e00a0d73205d6cc8f243a08ae62ffad8a96831f8
Gary Forte <gforte78@gmail.com>
2014-01-25 02:21:10 -0500

    Testing Again

--- androidtestapp/src/com/example/androidtestapp/MainActivity.java
+++ androidtestapp/src/com/example/androidtestapp/MainActivity.java
@@ -10,9 +10,14 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//for loop
+		for(int i=0; i<10; i++){
+			System.out.println(i);
+		}
 	}
 
-	//Gary put this comment here
+	
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -20,9 +25,5 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-     //Comment Here
-    
-    //Additional Comment Here
-
-	// another comment
+     
 }
