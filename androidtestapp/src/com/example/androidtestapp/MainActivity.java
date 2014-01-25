8bb139e2a8c6b24a6303075355cf643a37377b30
HardCoded <gforte78@gmail.com>
2014-01-25 03:33:18 -0500

    Update MainActivity.java

--- androidtestapp/src/com/example/androidtestapp/MainActivity.java
+++ androidtestapp/src/com/example/androidtestapp/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends Activity {
 		}
 		
 		System.out.println("I added this line from git hubs editor");
-		
+		System.out.println("I will fetch this file and store in local repository");
 	}
 
 	
